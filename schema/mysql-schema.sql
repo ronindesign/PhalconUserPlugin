5bac97cfc4189153c33c3113f62186572522d84d
Tim Turner <info@ronin-design.com>
2018-01-29 02:01:27 -0800

    Tables, columns now use utf8mb4 charset for 4-byte unicode

--- schema/mysql-schema.sql
+++ schema/mysql-schema.sql
@@ -1,13 +1,15 @@
+# noinspection SqlNoDataSourceInspectionForFile
+
 --
 -- Table structure for table `locations`
 --
 
 CREATE TABLE IF NOT EXISTS `locations` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `language` char(2) COLLATE utf8_bin DEFAULT NULL,
-  `formatted_address` varchar(160) COLLATE utf8_bin DEFAULT NULL,
-  `city` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `country` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `language` char(2) COLLATE utf8mb4_bin DEFAULT NULL,
+  `formatted_address` varchar(160) COLLATE utf8mb4_bin DEFAULT NULL,
+  `city` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `country` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
   `latitude` float(10,6) DEFAULT NULL,
   `longitude` float(10,6) DEFAULT NULL,
   `geo_point` point DEFAULT NULL,
@@ -15,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `locations` (
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `city` (`city`,`country`,`formatted_address`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -25,23 +27,23 @@ CREATE TABLE IF NOT EXISTS `locations` (
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) COLLATE utf8_bin DEFAULT NULL,
-  `first_name` varchar(32) COLLATE utf8_bin DEFAULT NULL,
-  `last_name` varchar(32) COLLATE utf8_bin DEFAULT NULL,
-  `email` varchar(48) COLLATE utf8_bin NOT NULL,
-  `password` varchar(128) COLLATE utf8_bin NOT NULL,
-  `facebook_id` varchar(20) COLLATE utf8_bin DEFAULT NULL,
-  `facebook_name` varchar(64) COLLATE utf8_bin DEFAULT NULL,
-  `facebook_data` text COLLATE utf8_bin,
-  `linkedin_id` varchar(64) COLLATE utf8_bin DEFAULT NULL,
-  `linkedin_name` varchar(64) COLLATE utf8_bin DEFAULT NULL,
-  `linkedin_data` text COLLATE utf8_bin,
-  `gplus_id` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `gplus_name` varchar(64) COLLATE utf8_bin DEFAULT NULL,
-  `gplus_data` text COLLATE utf8_bin,
-  `twitter_id` varchar(20) COLLATE utf8_bin DEFAULT NULL,
-  `twitter_name` varchar(64) COLLATE utf8_bin DEFAULT NULL,
-  `twitter_data` text COLLATE utf8_bin,
+  `name` varchar(64) COLLATE utf8mb4_bin DEFAULT NULL,
+  `first_name` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL,
+  `last_name` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL,
+  `email` varchar(48) COLLATE utf8mb4_bin NOT NULL,
+  `password` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `facebook_id` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL,
+  `facebook_name` varchar(64) COLLATE utf8mb4_bin DEFAULT NULL,
+  `facebook_data` text COLLATE utf8mb4_bin,
+  `linkedin_id` varchar(64) COLLATE utf8mb4_bin DEFAULT NULL,
+  `linkedin_name` varchar(64) COLLATE utf8mb4_bin DEFAULT NULL,
+  `linkedin_data` text COLLATE utf8mb4_bin,
+  `gplus_id` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `gplus_name` varchar(64) COLLATE utf8mb4_bin DEFAULT NULL,
+  `gplus_data` text COLLATE utf8mb4_bin,
+  `twitter_id` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL,
+  `twitter_name` varchar(64) COLLATE utf8mb4_bin DEFAULT NULL,
+  `twitter_data` text COLLATE utf8mb4_bin,
   `must_change_password` tinyint(1) DEFAULT NULL,
   `group_id` tinyint(3) unsigned NOT NULL,
   `banned` tinyint(1) NOT NULL COMMENT 'Deprecated field. Left behind for backwards compatibility. Use status column instead',
@@ -57,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   KEY `name` (`name`),
   KEY `group_id` (`group_id`),
   KEY `status` (`status`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=9 ;
 
 -- --------------------------------------------------------
 
@@ -68,13 +70,13 @@ CREATE TABLE IF NOT EXISTS `user` (
 CREATE TABLE IF NOT EXISTS `user_email_confirmations` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned NOT NULL,
-  `code` char(32) COLLATE utf8_bin NOT NULL,
+  `code` char(32) COLLATE utf8mb4_bin NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
   `confirmed` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=12 ;
 
 -- --------------------------------------------------------
 
@@ -85,11 +87,11 @@ CREATE TABLE IF NOT EXISTS `user_email_confirmations` (
 CREATE TABLE IF NOT EXISTS `user_failed_logins` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned DEFAULT NULL,
-  `ip_address` char(15) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `ip_address` char(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `attempted` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `usersId` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=37 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=37 ;
 
 -- --------------------------------------------------------
 
@@ -99,11 +101,11 @@ CREATE TABLE IF NOT EXISTS `user_failed_logins` (
 
 CREATE TABLE IF NOT EXISTS `user_groups` (
   `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `name` varchar(64) COLLATE utf8mb4_bin NOT NULL,
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `active` (`active`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
@@ -115,13 +117,13 @@ CREATE TABLE IF NOT EXISTS `user_notifications` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned NOT NULL,
   `object_id` bigint(20) NOT NULL,
-  `object_source` varchar(30) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT 'Object source can be a table. For example, articles. Then object_id is the id from article table',
-  `content` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `object_source` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT 'Object source can be a table. For example, articles. Then object_id is the id from article table',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `is_seen` tinyint(1) DEFAULT '0',
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4  AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -132,12 +134,12 @@ CREATE TABLE IF NOT EXISTS `user_notifications` (
 CREATE TABLE IF NOT EXISTS `user_password_changes` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned NOT NULL,
-  `ip_address` char(15) COLLATE utf8_bin NOT NULL,
-  `user_agent` varchar(255) COLLATE utf8_bin NOT NULL,
+  `ip_address` char(15) COLLATE utf8mb4_bin NOT NULL,
+  `user_agent` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=13 ;
 
 -- --------------------------------------------------------
 
@@ -148,11 +150,11 @@ CREATE TABLE IF NOT EXISTS `user_password_changes` (
 CREATE TABLE IF NOT EXISTS `user_permissions` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `group_id` tinyint(3) unsigned NOT NULL,
-  `resource` varchar(16) COLLATE utf8_bin NOT NULL,
-  `action` varchar(16) COLLATE utf8_bin NOT NULL,
+  `resource` varchar(16) COLLATE utf8mb4_bin NOT NULL,
+  `action` varchar(16) COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`id`),
   KEY `group_id` (`group_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=123 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=123 ;
 
 -- --------------------------------------------------------
 
@@ -174,7 +176,7 @@ CREATE TABLE IF NOT EXISTS `user_profile` (
   KEY `user_id` (`user_id`),
   KEY `home_location_id` (`home_location_id`),
   KEY `current_location_id` (`current_location_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4  AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -185,13 +187,13 @@ CREATE TABLE IF NOT EXISTS `user_profile` (
 CREATE TABLE IF NOT EXISTS `user_remember_tokens` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned NOT NULL,
-  `token` char(32) COLLATE utf8_bin NOT NULL,
-  `user_agent` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `token` char(32) COLLATE utf8mb4_bin NOT NULL,
+  `user_agent` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
   `created_at` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `token` (`token`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -202,13 +204,13 @@ CREATE TABLE IF NOT EXISTS `user_remember_tokens` (
 CREATE TABLE IF NOT EXISTS `user_reset_passwords` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned NOT NULL,
-  `code` varchar(48) COLLATE utf8_bin NOT NULL,
+  `code` varchar(48) COLLATE utf8mb4_bin NOT NULL,
   `created_at` datetime NOT NULL,
   `modified_at` datetime DEFAULT NULL,
   `reset` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -219,11 +221,11 @@ CREATE TABLE IF NOT EXISTS `user_reset_passwords` (
 CREATE TABLE IF NOT EXISTS `user_success_logins` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned NOT NULL,
-  `ip_address` char(15) COLLATE utf8_bin NOT NULL,
-  `user_agent` varchar(255) COLLATE utf8_bin NOT NULL,
+  `ip_address` char(15) COLLATE utf8mb4_bin NOT NULL,
+  `user_agent` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=238 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=238 ;
 
 --
 -- Constraints for dumped tables
